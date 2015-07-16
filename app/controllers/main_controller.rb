03ed1d3fddd8ff03b8dcd9a2116ce842e498862e
Vitalik <vitalikstetsenko@hotmail.com>
2015-07-17 00:25:52 +0300

    rand articles

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
     @not_image = 'http://paltelegraph.com/wp-content/uploads/2014/05/img_not_found.jpg'
-    @carusel = 6.times.map { | article | Category.first.articles.sample }
+    @carusel = 10.times.map { | article | Category.first.articles.sample }
     @last_news = 3.times.map { | article | Category.first.articles.sample }
     @last_news_small = 3.times.map { | article | Category.first.articles.sample }
   end
