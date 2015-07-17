a4672a82dd820ffaab0fab65af8d339c5cba106b
Vitalik <vitalikstetsenko@hotmail.com>
2015-07-17 18:01:17 +0300

    test 6

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,7 @@
 class MainController < ApplicationController
-  def index
-
+  def self.index
     @carusel = 10.times.map { | article | Category.first.articles.sample }
     @last_news = 3.times.map { | article | Category.first.articles.sample }
     @last_news_small = 3.times.map { | article | Category.first.articles.sample }
-
-    @article_images_any = article.article_images.any?
-    @true_article_images = article.article_images.first.image_url
-    @not_image = 'http://paltelegraph.com/wp-content/uploads/2014/05/img_not_found.jpg'
   end
 end
